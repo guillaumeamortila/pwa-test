c54e11282fa618fde254f50f4e64863ca46bbcab
Amortila Guillaume <guillaume.amortila@eemi.com>
2018-03-09 11:12:27 +0100

    first-push

---
+++ js/script.js
@@ -0,0 +1,15 @@
+// use of the local storage
+localStorage.setItem('test','test du local storage');
+console.log(localStorage.getItem('test'));
+
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', function() {
+    navigator.serviceWorker.register('../service-worker.js').then(function(registration) {
+      // Registration was successful
+      console.log('ServiceWorker registration successful with scope: ', registration.scope);
+    }, function(err) {
+      // registration failed :(
+      console.log('ServiceWorker registration failed: ', err);
+    });
+  });
+}
